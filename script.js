f5e85160a3dfd15b0cf00bc23eed8e41b439ee11
kalyandragoes <127746347+kalyandragoes@users.noreply.github.com>
2024-06-19 14:07:50 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,30 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const Perguntas = [
+    {
+        enunciado: "Para o sociologo Max Weber, os conflitos sociais seriam resultados das posiçoẽs assímetricas que os indivíduos ocupam na sociedades.Ela chamou de classe, estamento e partido ás diferentes eseras: Quais esas opções vocês conhecem?"
+        alternativas: [
+            "Sociais"
+            "Política"
+        ]
+    },
+    {
+    enunciado: "Pergunta 2"
+    alternativas: [
+        "Altenativa 1"
+        "Altenativa 2"
+        ]
+    },
+    {
+
+    enunciado: "Pergunta 3"
+    alternativas: [
+        "Altenativa 1"
+        "Altenativa 2"
+    ]
+    }
+]
