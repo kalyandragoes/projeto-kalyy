6dee2cad50237843198e28a8493d7aacc2a34c23
kalyandragoes <127746347+kalyandragoes@users.noreply.github.com>
2024-08-07 14:24:33 -0300

    kk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
+caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
@@ -6,73 +6,45 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Para o sociologo Max Weber, os conflitos sociais seriam resultados das posiçoẽs assimétricas que os indivíduos ocupam na sociedade. Ela chamou de clase, estamento e partido ás diferentes esferas:quais essas opçoẽs voces conhecem?",
-        alternativas: [
-            const       {
-                       texto: "Econômica !",
-                       afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
-                   },
-                   {
-                       texto: "Socias"
-                       afirmacao: "Quis saber como usar IA no seu dia a dia."
-                   },
-                   {
-                       texto: "Política "
-                       afirmacao: "Quis saber como usar IA no seu dia a dia."
-                   },
-               ],   
-           },
-           {
-               enunciado: "O programa auxílio Brasil criado em 2023, é um progroma de transferência de renda que reuniu outros auxílios existentes. Atualmente, o valor médio recebido por família e de R$ 600,00. É correto afirmar que o programa tem qual objetivo?",
-               alternativas: [
-                   {
-                       texto:"Reduzir ás taxas de mortalidade intantil"
-                       afirmacao: " Ao reduzir a taxa, estiver se o risco de um nascido. Vivo morrer antes de chegar a um ano de vida"
-                   },
-                   {
-                       texto: "Garantir o acesso a serviços essenciais",
-                       afirmacao: "Garantir o serviço essenciais garantir atividade de produção, distribuição e comercialização"
-                   }
-               ]
-           },
-           {
-               enunciado: "Quais as causas de desigualdade social?",
-               alternativas: [
-                   {
-                       texto: "Pobresa",
-                       afirmacao: "Pobresa seria muitas pessoas passando necessidade"
-                   },
-                   {
-                       texto: "Concentração de renda",
-                       afirmacao: "Concentração de renda seria dinheiro para áreas desnecessárias"
-                   }
-               ]
-           },
-       ];
-       
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+      enunciado: "Para o sociologo Max Weber, os conflitos socias seriam resultados das posiçoes assimetricos que os individuos ocupam na sociedades.Ela chamou de close,estamento e partido ás diferentes esferas.Quais essas opçoes vocẽs conhecem?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Econômica !",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Socias",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            },
+            {
+                texto: "Política ",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O programa auxílio Brasil criado em 2023, é um progroma de transferência de renda que reuniu outros auxílios existentes. Atualmente, o valor médio recebido por família e de R$ 600,00. É correto afirmar que o programa tem qual objetivo?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto:"Reduzir ás taxas de mortalidade intantil",
+                afirmacao: " Ao reduzir a taxa, estiver se o risco de um nascido. Vivo morrer antes de chegar a um ano de vida"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Garantir o acesso a serviços essenciais",
+                afirmacao: "Garantir o serviço essenciais garantir atividade de produção, distribuição e comercialização"
+            }
+        ]
+    },
+    {
+        enunciado: "Quais as causas de desigualdade social?",
+        alternativas: [
+            {
+                texto: "Pobresa",
+                afirmacao: "Pobresa seria muitas pessoas passando necessidade"
+            },
+            {
+                texto: "Concentração de renda",
+                afirmacao: "Concentração de renda seria dinheiro para áreas desnecessárias"
             }
         ]
     },
